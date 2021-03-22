801a5e0f03613e437c63ba5a924b69eb68a8fac1
micky-it <78507588+micky-it@users.noreply.github.com>
2021-03-22 16:42:09 +0100

    Fixed start-compiling.sh

--- start-compiling.sh
+++ start-compiling.sh
@@ -7,12 +7,12 @@ echo -e "Hello $NAMENAME !"
 echo -n "Press any key to start compiling OpenTom..."
 read VARVARINPUT
 
-if [[ -z "$VAR2" ]]
+if [[ -z "$VARVARINPUT" ]]
 then
     source get_cross_env.sh
     make
     echo -e "If you are getting an error like 'x86_64-unknown-linux-gnu'
-             \bdo linux32 make"
+do linux32 make"
           
 fi
     
